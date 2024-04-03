e8eb0b1e7d88ce02d1f0fdc445c5232a89781438
aamir <aamirkhan@engineer.com>
2024-04-03 12:34:33 +0500

    fix(src): fixed the back button and image resize in articlePreview

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -1,14 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
 
 const ArticlePreview = () => {
-    return (
-        <View>
-            <Text>ArticlePreview</Text>
-        </View>
-    )
-}
+	return (
+		<View className="min-h-screen">
+			<View
+				className="h-2/6 relative overflow-hidden"
+				style={{
+					borderBottomLeftRadius: 15,
+					borderBottomRightRadius: 15,
+				}}
+			>
+				<Image
+					className="w-full h-full"
+					source={{
+						uri: "https://www.geosuper.tv/assets/uploads/updates/2022-11-20/20187_400599_updates.jpg",
+					}}
+				/>
+				<View className=" h-6 w-16 absolute top-4 left-2 flex flex-row justify-evenly items-center">
+					<Image
+						className="w-4 aspect-square"
+						source={{
+							uri: "https://static.thenounproject.com/png/517807-200.png",
+						}}
+					/>
+					<Text
+						className="text-base"
+						style={{ fontFamily: "montserrat-semibold" }}
+					>
+						Back
+					</Text>
+				</View>
+			</View>
 
-export default ArticlePreview
+			{/* below picture area */}
+			<View className="mx-2">
+				{/* title */}
+				<Text
+					className="text-2xl mt-10"
+					style={{ fontFamily: "rufina-bold" }}
+				>
+					How the Politicle parties effect the price of BTC.
+				</Text>
 
-const styles = StyleSheet.create({})
+				{/* author and date */}
+				<View className="w-full flex flex-row justify-between items-center mt-6">
+					<View className="flex flex-row">
+						<Text
+							className="text-sm"
+							style={{ fontFamily: "montserrat-light" }}
+						>
+							written by
+						</Text>
+						<Text
+							className="text-sm"
+							style={{ fontFamily: "montserrat-bold" }}
+						>
+							{" "}
+							Jack Richer
+						</Text>
+					</View>
+					<Text
+						className="text-sm"
+						style={{ fontFamily: "montserrat-light" }}
+					>
+						14 MAR 2024
+					</Text>
+				</View>
+				{/* line */}
+				<View className="w-full border-b-2 mt-2 border-gray-200"></View>
+
+				<Text
+					className="mt-8 text-sm"
+					style={{ fontFamily: "montserrat-regular" }}
+				>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Maiores delectus provident suscipit architecto molestiae
+					deserunt in? Earum, itaque. Nostrum amet quo voluptas
+					eligendi aspernatur deleniti officia, nulla veritatis libero
+					quaerat! Nulla iusto et porro minima omnis quas eaque autem,
+					accusamus natus ipsum facilis earum veniam necessitatibus
+					incidunt illum temporibus explicabo magni tenetur quis non.
+					Officiis dolorum provident quae quasi autem? Dicta aperiam
+					cum, numquam et quod aspernatur illum fugit sequi debitis
+					porro modi impedit soluta sunt ex minus officia iure
+					dignissimos tenetur autem ipsam ratione ipsum laboriosam!
+					Saepe, numquam. Enim explicabo eveniet fugit ipsum fuga
+					totam, atque molestiae voluptatibus eligendi!
+				</Text>
+			</View>
+		</View>
+	);
+};
+
+export default ArticlePreview;
+
+const styles = StyleSheet.create({});
