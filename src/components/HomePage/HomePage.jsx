a5da12256847731a54eb9a4c40df2364358d7239
aamir <aamirkhan@engineer.com>
2024-04-03 11:29:32 +0500

    feat: added fonts

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,50 +1,94 @@
 import { Button, ScrollView, StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import ArticleCard from '../ArticleCard/ArticleCard';
 const HomePage = () => {
-    const [fontLoaded] = useFonts({
+    const [loaded, error] = useFonts({
         "rufina-regular": require("../../assets/fonts/Rufina-Regular.ttf"),
         "rufina-bold": require("../../assets/fonts/Rufina-Bold.ttf"),
-        "montserrat-normal": require("../../assets/fonts/Montserrat-normal.ttf")
+        "montserrat-normal": require("../../assets/fonts/Montserrat-normal.ttf"),
+        "montserrat-bold": require("../../assets/fonts/Montserrat-Bold.ttf"),
+        "montserrat-extrabold": require("../../assets/fonts/Montserrat-ExtraBold.ttf"),
+        "montserrat-light": require("../../assets/fonts/Montserrat-Light.ttf"),
+        "montserrat-medium": require("../../assets/fonts/Montserrat-Medium.ttf"),
+        "montserrat-regular": require("../../assets/fonts/Montserrat-Regular.ttf"),
+        "montserrat-semibold": require("../../assets/fonts/Montserrat-SemiBold.ttf")
     })
+
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
-        <View className="mt-16 mx-2">
-            <Text style={styles.welcomeHeading}>Welcome, 👋</Text>
+        <View className=" mx-2">
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles.welcomeHeading}>Welcome, 👋</Text>
 
-            <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-4 py-4">
-                <Text style={styles.categoryBtn}>Latest</Text>
-                <Text style={[styles.categoryBtn, styles.selectedCategoryBtn]}>Technology</Text>
-                <Text style={styles.categoryBtn}>Finance</Text>
-                <Text style={styles.categoryBtn}>Politics</Text>
-                <Text style={styles.categoryBtn}>Programming</Text>
-            </ScrollView>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-4 py-4">
+                    <Text style={styles.categoryBtn}>Latest</Text>
+                    <Text style={[styles.categoryBtn, styles.selectedCategoryBtn]}>Technology</Text>
+                    <Text style={styles.categoryBtn}>Finance</Text>
+                    <Text style={styles.categoryBtn}>Politics</Text>
+                    <Text style={styles.categoryBtn}>Programming</Text>
+                </ScrollView>
+                {/* line */}
+                <View className="w-full border-b-2 border-gray-200"></View>
 
-            {/* image and the top article */}
-            <View className="overflow-hidden h-[224px] mt-6 relative" style={styles.topArticle}>
-                <Image className="w-full h-full" source={{ uri: "https://plus.unsplash.com/premium_photo-1681492405224-b787ee736768?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
+                {/* image and the top article */}
+                <View className="overflow-hidden h-[224px] mt-6 relative" style={styles.topArticle}>
+                    <Image className="w-full h-full" source={{ uri: "https://plus.unsplash.com/premium_photo-1681492405224-b787ee736768?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
 
-                {/*Heading and the author of the top article  */}
-                <View className="z-10 absolute bottom-0 left-0 w-full h-20 px-2">
-                    <View className=" w-full rounded-md h-9 flex justify-center items-center" style={styles.blogHeading}>
-                        <Text className=" w-full text-left ml-2 text-base" style={{ fontFamily: "montserrat-normal" }}>How the political parties are effecting....</Text>
-                    </View>
-                    <View className="flex flex-row justify-end mt-2 items-center">
-                        <View className="flex flex-row gap-2 items-center">
-                            <Text className="text-sm" style={{ fontFamily: "montserrat-normal" }}>Nicolas johns.</Text>
-                            <Image className="w-6 h-6 rounded-full" source={{ uri: "https://plus.unsplash.com/premium_photo-1681492405224-b787ee736768?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
+                    {/*Heading and the author of the top article  */}
+                    <View className="z-10 absolute bottom-0 left-0 w-full h-20 px-2">
+                        <View className=" w-full rounded-md h-9 flex justify-center items-center" style={styles.blogHeading}>
+                            <Text className=" w-full text-left ml-2 text-lg" style={{ fontFamily: "montserrat-regular" }}>How the political parties are effecting....</Text>
+                        </View>
+                        <View className="flex flex-row justify-end mt-2 items-center">
+                            <View className="flex flex-row gap-2 items-center">
+                                <Text className="text-sm" style={{ fontFamily: "montserrat-light" }}>Nicolas johns.</Text>
+                                <Image className="w-6 h-6 rounded-full" source={{ uri: "https://plus.unsplash.com/premium_photo-1681492405224-b787ee736768?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
+                            </View>
                         </View>
                     </View>
                 </View>
-            </View>
 
 
-            {/* Top articles */}
-            <Text className="bg-red-400 my-4" style={{ fontFamily: "rufina-regular", fontSize: 28 }}>
-                Latest From Technology
-            </Text>
+                {/* Top articles */}
+                <Text className=" my-4" style={{ fontFamily: "rufina-regular", fontSize: 28 }}>
+                    Latest From Technology
+                </Text>
 
 
-            {/* Articles */}
+                {/* Articles */}
+                <View className="flex">
+                    <View className="mb-4">
+                        <ArticleCard />
+                    </View>
+                    <View className="mb-4">
+                        <ArticleCard />
+                    </View>
+                    <View className="mb-4">
+                        <ArticleCard />
+                    </View>
+                    <View className="mb-4">
+                        <ArticleCard />
+                    </View>
+                    <View className="mb-4">
+                        <ArticleCard />
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
@@ -58,19 +102,19 @@ const styles = StyleSheet.create({
     },
     welcomeHeading: {
         fontSize: 32,
-        fontFamily: "rufina-regular"
+        fontFamily: "rufina-bold"
     },
     catagories: {
         marginTop: 24
     },
     categoryBtn: {
         marginHorizontal: 16,
-        fontFamily: "montserrat-normal",
+        fontFamily: "montserrat-regular",
         fontSize: 14,
         fontWeight: "600",
     },
     selectedCategoryBtn: {
-        fontWeight: "bold"
+        fontFamily: "montserrat-bold"
     },
     topArticle: {
         borderRadius: 15
