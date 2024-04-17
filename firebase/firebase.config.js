a4e2d531973277bb098e26f08ee549396310673b
aamir <aamirkhan@engineer.com>
2024-04-17 13:05:26 +0500

    added firebase

---
+++ firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgGrlOHWpgZai_RjEo3AiGw8IDM-Xh1ks",
+    authDomain: "blogs-app-b1036.firebaseapp.com",
+    projectId: "blogs-app-b1036",
+    storageBucket: "blogs-app-b1036.appspot.com",
+    messagingSenderId: "354611676521",
+    appId: "1:354611676521:web:bbeafa7675e39cf03ba9f8",
+    measurementId: "G-4QCRM9Q6HF"
+};
+
+
+export const app = initializeApp(firebaseConfig);
