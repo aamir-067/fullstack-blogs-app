2fe46f2c47e9bb3517b7452f59ecc02b6b1b5bef
aamir <aamirkhan@engineer.com>
2024-04-18 15:21:50 +0500

    fix(components): fixed icons in footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,5 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
-import HomeSVG from "../../assets/home.svg"
-import BlogSVG from "../../assets/blog-pencil.svg"
-import ProfileSVG from "../../assets/user.svg"
 import { Link } from 'expo-router'
 
 const Footer = () => {
@@ -10,21 +7,25 @@ const Footer = () => {
         <View className="w-full flex flex-row z-50 justify-evenly items-center" style={{ backgroundColor: "#203406", height: "7%", width: "100%" }}>
             <Link href="/">
                 <View className=" flex items-center justify-center">
-                    <View className="w-6 aspect-square bg-white rounded-full"></View>
+                    <View className="w-6 aspect-square rounded-full">
+                        <Image className="w-full h-full" source={require("../../assets/home.png")} />
+                    </View>
                     <Text className="text-xs mt-1 text-white" style={{ fontFamily: "montserrat-medium" }}>Home</Text>
                 </View>
             </Link>
             <Link href="/article/preview">
                 <View className="flex items-center justify-center">
-                    <View className="w-6 aspect-square bg-white rounded-full">
-                        <Image className="w-full h-full" source={require("../../assets/home.svg")} />
+                    <View className="w-6 aspect-square rounded-full">
+                        <Image className="w-full h-full" source={require("../../assets/blog-write.png")} />
                     </View>
                     <Text className="text-xs mt-1 text-white" style={{ fontFamily: "montserrat-medium" }}>Publish</Text>
                 </View>
             </Link>
             <Link href={"/profile"}>
                 <View className=" flex items-center justify-center">
-                    <View className="w-6 aspect-square bg-white rounded-full"></View>
+                    <View className="w-6 aspect-square rounded-full">
+                        <Image className="w-full h-full" source={require("../../assets/person.png")} />
+                    </View>
                     <Text className="text-xs mt-1 text-white" style={{ fontFamily: "montserrat-medium" }}>Profile</Text>
                 </View>
             </Link>
